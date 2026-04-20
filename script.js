a8b29dd6d6310e56bac7d046d9196f2718ce6415
Nagargoje Abhishek <nagargojeabhishek96@gmail.com>
2026-04-20 19:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Create the student object
+
 const student = {
   name: "John"
 };
 
-// Function to get all keys
-function getKeys(obj) {
-  return Object.keys(obj);
-}
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
 
-// Test
-console.log(getKeys(student)); // ["name"]
+
+console.log(getKeys(student)); 
