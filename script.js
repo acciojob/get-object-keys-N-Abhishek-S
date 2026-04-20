b145d442bb056833bf4e4e469af3f3275def69d8
Nagargoje Abhishek <nagargojeabhishek96@gmail.com>
2026-04-20 19:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let student = {
+	  name: "Abhishek",
+  country: "India",
+  role: "Student"
+
+}
+
+function getKeys(obj){
+	 return Object.keys(obj);
+}
+console.log(getKeys(student));
